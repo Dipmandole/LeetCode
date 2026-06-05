9436fb7f0944322034a5df7cb5f0c8b7af0be6c5
Dipmandole <dipmandole1602@gmail.com>
2026-06-05 08:10:59 +0530

    Time: 49 ms (62.33%), Space: 55.5 MB (96.44%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,26 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+        let original = init
+    return {
+        increment: () =>{
+            return ++init;
+        },
+        decrement: () =>{
+            return --init;
+        },
+        reset: () =>{
+            init = original
+            return original;
+        },
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
