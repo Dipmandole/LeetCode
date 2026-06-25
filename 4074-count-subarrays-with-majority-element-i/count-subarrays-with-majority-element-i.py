cf7303c265acf9cb147c4aecab2e838f3919b218
Dipmandole <dipmandole1602@gmail.com>
2026-06-25 09:52:21 +0530

    Time: 2290 ms (27.91%) | Memory: 12.7 MB (9.30%) - LeetSync

---
+++ 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def countMajoritySubarrays(self, nums, target):
+        n = len(nums)
+        ans = 0
+
+        for i in range(n):
+            count = 0
+            for j in range(i, n):
+                if nums[j] == target:
+                    count += 1
+
+                length = j - i + 1
+
+                if count > length // 2:
+                    ans += 1
+        return ans        
+        
+        
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
