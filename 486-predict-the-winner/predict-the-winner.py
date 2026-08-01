c31f1ce6ebacaa8445c8ea624a230c4f6ea9f53a
Dip Mandole <dipmandole1602@gmail.com>
2026-08-01 11:09:16 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (45.09%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.py
+++ 486-predict-the-winner/predict-the-winner.py
@@ -4,29 +4,20 @@ class Solution(object):
 
         dp = [[-1] * n for _ in range(n)]
 
-        def solve(left, right):
-            if left > right:
-                return 0
+        def maxDiff(left, right):
             if left == right:
                 return nums[left]
-
+            
             if dp[left][right] != -1:
                 return dp[left][right]
             
-            take_left = nums[left] + min(solve(left + 2, right),
-            solve(left + 1, right - 1))
+            take_left = nums[left] - maxDiff(left + 1, right)
+            take_right = nums[right] - maxDiff(left, right - 1)
 
-            take_right = nums[right] + min(solve(left, right - 2),
-            solve(left + 1, right - 1))
 
             dp[left][right] = max(take_left, take_right)
             return dp[left][right]
-        total = sum(nums)
-
-        player1 = solve(0, n - 1)
-        player2 = total - player1
-
-        return player1 >= player2
+        return maxDiff(0, n - 1) >= 0
 
 
         """
